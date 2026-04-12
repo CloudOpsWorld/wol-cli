dfa76ad06ced504b95d7d6eb21a5a4306ce1911d
cow-lucian <107362462+cow-lucian@users.noreply.github.com>
2026-04-13 02:52:14 +0300

    release v1.0.1 (#14)

--- src/wol_cli/__init__.py
+++ src/wol_cli/__init__.py
@@ -1,6 +1,6 @@
 """Wake-on-LAN magic packet sender with VLAN support."""
 
-__version__ = "1.0.0"
+__version__ = "v1.0.1"
 
 from .packet import build_magic_packet, parse_mac
 from .transport import broadcast_from_network, send_dot1q, send_udp
